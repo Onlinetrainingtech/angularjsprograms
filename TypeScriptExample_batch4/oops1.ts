cf6b32ee2344df0f1d9fefc2f657dd8bd7111ed0
OnlineTrainer <mohamedtechdata@gmail.com>
2021-02-23 08:20:48 +0530

    23-02-2021

---
+++ TypeScriptExample_batch4/oops1.ts
@@ -0,0 +1,20 @@
+class Employee
+{
+    employeeid:number;
+    //constructor
+    constructor(employeeid:number)
+    {
+        this.employeeid=employeeid
+    }
+    //function
+    disp():void{
+
+        console.log("Your EmployeeId is::"+this.employeeid)
+    }
+}
+
+var obj=new Employee(101)
+
+//access the object
+
+obj.disp()
---
+++ TypeScriptExample_batch4/typescriptp6.ts
@@ -0,0 +1,5 @@
+function sample()
+{
+    console.log("function called..")
+}
+sample();
