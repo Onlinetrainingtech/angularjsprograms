1cc1bc196b56887f62dcaf0460f0252cef565def
OnlineTrainer <mohamedtechdata@gmail.com>
2022-10-14 08:17:45 +0530

    14-10-2022

--- AngularProgramsB12/AngularApp2/src/app/app.component.ts
+++ AngularProgramsB12/AngularApp2/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularApp2';
+
+  employeename=["mohamed","azar","raja"]
+
+  isflag=false;
+
+  myclickfunction(event:any)
+  {
+    alert("Button is Clicked")
+    console.log(event)
+  }
+  changeemp(event:any)
+  {
+    alert("You have select employee")
+    console.log(event)
+  }
 }
