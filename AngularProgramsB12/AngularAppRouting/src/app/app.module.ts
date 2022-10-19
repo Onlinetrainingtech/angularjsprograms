5d18d3447c6750d76ea74789227b6d2c7f0e0978
OnlineTrainer <mohamedtechdata@gmail.com>
2022-10-19 08:18:48 +0530

    19-10-2022

--- AngularProgramsB12/AngularAppRouting/src/app/app.module.ts
+++ AngularProgramsB12/AngularAppRouting/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NewComponentComponent } from './new-component/new-component.component';
+import { EmployeeComponent } from './employee/employee.component';
+import {RouterModule} from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComponentComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+   /*RouterModule.forRoot([
+    {
+    path:'n1',
+    component:NewComponentComponent,
+   },
+   {
+    path:'n2',
+    component:EmployeeComponent,
+   }
+   ])*/
   ],
   providers: [],
   bootstrap: [AppComponent]
