7ca291bd84981aad327d7852e4eb3c6c6c7bbc82
OnlineTrainer <mohamedtechdata@gmail.com>
2022-12-14 22:33:25 +0530

    14-12-22

--- AngularBatch14/AngularP2/src/app/app.component.ts
+++ AngularBatch14/AngularP2/src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template:`<p [innerHtml]='uname'></p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'AngularP2';
+  uname:string="Abdur";
 }
