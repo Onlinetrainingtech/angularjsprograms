83e2e4260f4f82af4e5bf01ab4cdcecd49c465d1
OnlineTraning <57015977+Onlinetrainingtech@users.noreply.github.com>
2023-04-18 07:52:17 +0530

    18-04-2023

---
+++ TypeScriptExample_batch21/typescriptp5.ts
@@ -0,0 +1,4 @@
+var arr_1=new String("welcome")
+var arr_2=new String("hai")
+var arr=arr_1.concat(arr_2.toString())
+console.log(arr)
---
+++ TypeScriptExample_batch21/typescriptp6.ts
@@ -0,0 +1,9 @@
+var num:number=5
+if(num>0)
+{
+    console.log("NUmber is Postive")
+}
+else
+{
+    console.log("Number is Negative")
+}
